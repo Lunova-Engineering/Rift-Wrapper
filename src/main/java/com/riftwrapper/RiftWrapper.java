898ad6ee4ef03d9b3c9953eb0c35d8676513540f
Drake Forness <drakeforness@gmail.com>
2023-08-31 09:41:05 -0700

    - Added Maven framework - Placed a single file to test run configurations

---
+++ src/main/java/com/riftwrapper/RiftWrapper.java
@@ -0,0 +1,8 @@
+package com.riftwrapper;
+
+public class RiftWrapper {
+
+    public static void main(String[] args) {
+        System.out.println("First Commit, testing build structure and project structure!");
+    }
+}
